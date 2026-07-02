c49608fba08aeac2fb618e74e743cd02332643c4
Eshaan Singh Deo <84272966+DevRexxxx@users.noreply.github.com>
2026-07-02 15:38:49 +0530

    Delete Music_Player directory

--- Music_Player/Buzz/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls import static
-from . import views
-from django.urls import path
-
-app_name='buzz'
-urlpatterns=[
-    path('', views.index, name='index'),
-]   
